b808c214d68162d875b9a0f7756964130fea41f7
NAZRIN21 <nazrinnazz2001@gmail.com>
2022-11-15 21:33:10 +1400

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { ViewDetailsComponent } from './view-details/view-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RegisterComponent},
+  { path: '', component: ViewDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
